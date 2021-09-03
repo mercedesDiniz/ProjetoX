9c1e548876050ff30df64c3419c5dbfa4d211229
mercedesDiniz <mercy.diniz82@gmail.com>
2021-09-03 10:33:48 -0300

    Codigo teste do Sistema de Detecção Auxiliar Infravermelho

--- test/Teste01_SistemaAuxiliardeDetecçãoInfravermelho/Teste01_SistemaAuxiliardeDetecçãoInfravermelho.ino
+++ test/Teste01_SistemaAuxiliardeDetecçãoInfravermelho/Teste01_SistemaAuxiliardeDetecçãoInfravermelho.ino
@@ -1,5 +1,4 @@
-/* Teste 01 - Sistema Auxiliar de Detecção Infravermelho (usando Led Emissor IR e Fototransistor IR)  (NÃO TESTADO)
-
+/* Teste 01 - Sistema Auxiliar de Detecção Infravermelho (usando Led Emissor IR e Fototransistor IR)  (NÃO TESTADO E PUCO PROVAVEL Q DE CERTO)
 - Proposta: Utilizar o Led Emissor IR e Fototransistor IR em conjunto com o Arduino e enviar o sinal IR do emissor para o receptor, e sempre que o sinal for interrompido significa que algum obstaculo/pessoa passou pelo perimetro. Com um comjunto formado por dois pares desse sistema podemos determida o sentido da locomoção (se a pessoa esta entrando ou saindo) se instalado em uma abertura (como porta ou janela). 
 
 *OBS.01: Quanto maior for a distância do emissor em relação ao receptor, maior será a leitura feita pelo pino analógico.
@@ -12,7 +11,7 @@ const int pinoFotot01 = A2; // Pino Analogico
 const int pinoFotot02 = A3; // Pino Analogico
 
 int controle_pessoas = 1;  // Ao ligar o dispositivo e sair, esse valor será ZERO! (consederando que apenas uma pessoa saiu apos o dispositivo ser ligado). Essa condição pode ter que ser adaptada dependendo da situação ou local de instalação (se for instalado em uma janela, pode ser iniciado ja como ZERO).
-
+int cont_F01, cont_F02 = 0; // Variaveis de controle para auxiliar na determinação da ordem de disparo do sistema.
 
   void setup() {   
     Serial.begin(9600); // Iniciando comunicaçao Serial   
@@ -21,26 +20,48 @@ int controle_pessoas = 1;  // Ao ligar o dispositivo e sair, esse valor será ZE
   }
   
   void loop() {
-    // ANALIZANDO O PRIMEIRO PAR(LedIR01 e F01 que devem está mais para a parte de fora, logo se ele for acionado primeiro é pq a pesoa está ENTRANDO.)	
-    if(analogRead(pinoFotot01) < 800){  // Recebendo sinal infravermelho do F01! (valor varia de acordo com a distancia do Led01 e F01,e pode ter que ser calibrado).
-    	Serial.println("Recebendo sinal infravermelho no F01"); // TUDO OK! 	
-    }
-    else{  // Sinal infravermelho do F01 interrompido
-        controle_pessoas = controle_pessoas + 1;
-	Serial.println("Sinal infravermelho interrompido no F01 = ALGUEM ENTROU"); // ALGUEM ENTROU!
+// ANALIZANDO O PRIMEIRO PAR(LedIR01 e F01 que devem está mais para a parte de FORA, logo se ele for acionado primeiro é pq a pesoa está ENTRANDO.)
+// ANALIZANDO O SEGUNDO PAR(LedIR02 e F02 que devem está mais para a parte de DENTRO, logo se ele for acionado primeiro é pq a pesoa está SAINDO.)	
+    
+    if(cont_F01 == 0 && cont_F02 = 0){	
+	    if(analogRead(pinoFotot01) > 800){   // Sinal infravermelho do F01 interrompido! (valor varia de acordo com a distancia do Led01 e F01,e pode ter que ser calibrado).
+	    	Serial.println("Sinal infravermelho interrompido no F01");
+	    	cont_F01 = 1; // o F1 foi o Primeiro à disparar
+	    	if(analogRead(pinoFotot02) > 800){  // Sinal infravermelho do F02 interrompido! (valor varia de acordo com a distancia do Led02 e F02,e pode ter que ser calibrado).
+	    	Serial.println("Sinal infravermelho interrompido no F02");   
+	    	cont_F02 = 2; //  o F2 foi o Sugundo à disparar
+		}  		
+	    }
     }
     
-    // ANALIZANDO O SEGUNDO PAR(LedIR02 e F02 que devem está mais para a parte de dentro, logo se ele for acionado primeiro é pq a pesoa está SAINDO.)	
-    if(analogRead(pinoFotot01) < 800){  // Recebendo sinal infravermelho do F02! (valor varia de acordo com a distancia do Led02 e F02,e pode ter que ser calibrado).
-    	Serial.println("Recebendo sinal infravermelho no F02"); // TUDO OK! 	
-    }
-    else{  // Sinal infravermelho do F01 interrompido
-        controle_pessoas = controle_pessoas - 1;
-	Serial.println("Sinal infravermelho interrompido no F02 = ALGUEM SAIU"); // ALGUEM SAIU!
+// OBS.: Não tenho certeza se o bloco de instruções de baixo não vai sobreescrever o de cima! A logica pode ter que ser modificada.
+
+    if(cont_F01 == 0 && cont_F02 = 0){
+	    if(analogRead(pinoFotot02) > 800){   // Sinal infravermelho do F02 interrompido! (valor varia de acordo com a distancia do Led01 e F01,e pode ter que ser calibrado).
+	    	Serial.println("Sinal infravermelho interrompido no F02");
+	    	cont_F02 = 1; //  o F2 foi o Primeiro à disparar
+	    	if(analogRead(pinoFotot02) > 800){  // Sinal infravermelho do F01 interrompido! (valor varia de acordo com a distancia do Led02 e F02,e pode ter que ser calibrado).
+	    	Serial.println("Sinal infravermelho interrompido no F01");   
+	    	cont_F01 = 2; //  o F1 foi o Sugundo à disparar
+		}  		
+	    }
     }
     
-    Serial.print("NÚMERO DE PESSOAS VERIFICADO = ");
-    Serial.println(controle_pessoas);
+// Analizando a ordem para determinar se o movimento foi de ENTRADA ou SAIDA:
+   if (cont_F01 == 1 && cont_F02 = 2){
+  	Serial.println("ALGUEM ENTROU"); // ALGUEM ENTROU!
+   } 
+   if (cont_F01 == 2 && cont_F02 = 1){
+  	Serial.println("ALGUEM SAIU"); // ALGUEM SAIU!
+   } 
+   Serial.print("NÚMERO DE PESSOAS VERIFICADO = ");
+   Serial.println(controle_pessoas);	
+// Reiniciando os parametros de controle:
+   cont_F01 = 0;
+   cont_F02 = 0;
+  
+ }
+        
   }
   
 
